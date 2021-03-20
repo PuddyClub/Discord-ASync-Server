15a312727f2010255095dce8019e03e531a451d8
Yasmin Seidel <jackiedevpe@outlook.com.br>
2021-03-19 21:22:51 -0300

    firebase close added

--- index.js
+++ index.js
@@ -1,18 +1,36 @@
 // Prepare Module
 const firebase = require('firebase');
 const express = require('express');
+const ON_DEATH = require('death');
 
 // App
 const app = {};
 
 module.exports = {
 
-    // Start Firebase
-    firebase: function (cfg) { 
-        firebase.initializeApp(cfg); 
-        app.auth = firebase.auth(); 
-        app.db = firebase.database(); 
-        return firebase; 
+    // Firebase
+    firebase: {
+
+        // Start
+        start: function (cfg) {
+            firebase.initializeApp(cfg);
+            app.auth = firebase.auth();
+            app.db = firebase.database();
+            return firebase;
+        },
+
+        // Login Firebase
+        login: function (token) { return app.auth.signInWithCustomToken(token); }
+
     }
 
 };
+
+// ON Death
+ON_DEATH(async function (signal, err) {
+
+    // Close Firebase Connection
+    try { await firebase.auth().signOut(); } catch (err) { }
+    return;
+
+});
