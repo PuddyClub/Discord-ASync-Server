050aab905f80d355e29c67877a78bbc5006eff75
Yasmin Seidel <jackiedevpe@outlook.com.br>
2021-03-24 01:39:43 -0300

    next

--- index.js
+++ index.js
@@ -80,7 +80,7 @@ const appModule = {
                             }
 
                             // Debug
-                            else { next(); }
+                            else { return next(); }
 
                         },
 
