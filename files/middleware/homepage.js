35f8d2c558523b4d26135d8856e21f55b8515eff
Yasmin Seidel <jackiedevpe@outlook.com.br>
2021-03-29 22:59:00 -0300

    package fixed completely

--- files/middleware/homepage.js
+++ files/middleware/homepage.js
@@ -3,9 +3,6 @@ module.exports = async function (req, res, webCfg, web, app) {
     // Exist Session
     if (req.discord_session.user) {
 
-        // Test
-        console.log(req);
-
         // Get User Values
         const index = app.users.find(user => user.id === req.discord_session.user.id);
 
