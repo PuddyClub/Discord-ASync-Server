1029fb67c5255fe4715a2ced568896ba8c2e2737
Yasmin Seidel <jackiedevpe@outlook.com.br>
2021-03-31 14:36:13 -0300

    page update

--- files/client/homepage.js
+++ files/client/homepage.js
@@ -17,10 +17,10 @@ $(() => {
     const toolsCreator = {
 
         // Card Row
-        cardRow: function (title = 'Example', info = '???', icon = 'fas fa-question', size = 'col-xl-4 col-md-6 mb-4') {
+        cardRow: function (title = 'Example', info = '???', icon = 'fas fa-question', color = 'primary', size = 'col-xl-4 col-md-6 mb-4') {
 
             return $('<div>', { class: size }).append(
-                $('<div>', { class: 'card border-left-primary shadow h-100 py-2' }).append(
+                $('<div>', { class: 'card border-left-' + color + ' shadow h-100 py-2' }).append(
                     $('<div>', { class: 'card-body' }).append(
                         $('<div>', { class: 'row no-gutters align-items-center' }).append(
 
@@ -28,10 +28,10 @@ $(() => {
                             $('<div>', { class: 'col mr-2' }).append(
 
                                 // Title
-                                $('<div>', { class: 'text-xs font-weight-bold text-primary text-uppercase mb-1', id: 'title' }).text(title),
+                                $('<div>', { class: 'text-xs font-weight-bold text-' + color + ' text-uppercase mb-1', id: 'title' }).text(title),
 
                                 // Info
-                                $('<div>', { class: 'h5 mb-0 font-weight-bold text-gray-800', id: 'info'}).text(info)
+                                $('<div>', { class: 'h5 mb-0 font-weight-bold text-gray-800', id: 'info' }).text(info)
 
                             ),
 
@@ -65,13 +65,22 @@ $(() => {
                 $('#ds_bot_' + bot.id).parent().removeClass('active').css('pointer-events', '');
 
                 // Rest App Place
+                $('#panel_title').text(tinyLang.dashboard);
+                $('.config-list').removeClass('active');
+                $('#dashboard.config-list').addClass('active');
                 $('#app').empty().append(
 
                     // Statics
                     $('<div>', { id: 'statistical_table', class: 'row' }).append(
 
+                        // Bot Name
+                        toolsCreator.cardRow('ID', botID, 'fas fa-robot').attr('id', 'bot_id'),
+
                         // Servers
-                        toolsCreator.cardRow('Servers', '???', 'fas fa-server').attr('id', 'server_count')
+                        toolsCreator.cardRow('Servers', '???', 'fas fa-server', 'info').attr('id', 'server_count'),
+
+                        // Channels
+                        toolsCreator.cardRow('Channels', '???', 'fas fa-th-list', 'info').attr('id', 'channel_count')
 
                     )
 
@@ -90,7 +99,8 @@ $(() => {
 
     });
 
-    // Guilds List
+    // Update Info
     socket.on('dsBot_serverCount', (count) => { $('#server_count #info').text(count); });
+    socket.on('dsBot_channelCount', (count) => { $('#channel_count #info').text(count); });
 
 });
