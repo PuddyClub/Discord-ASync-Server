71dd47eeff0b4d0a04b93f112cfad4aad2275579
Yasmin Seidel <jackiedevpe@outlook.com.br>
2021-03-31 00:49:34 -0300

    socket fixed

--- files/client/homepage.js
+++ files/client/homepage.js
@@ -2,5 +2,23 @@ $(() => {
 
     // Socket Connection
     const socket = io.connect();
+    let firstTime = true;
+    let user = null;
+    $.LoadingOverlay("show", { background: "rgba(0,0,0, 0.5)" });
+
+    // Connected
+    socket.on('discordConnected', user => {
+        if (firstTime) { firstTime = false; $.LoadingOverlay('hide'); }
+        user = user;
+    });
+
+    // Select Bot
+    $('[id^="ds_bot_"]').click(function() {
+
+        // Get Bot ID
+        const botID = $(this).attr('id').substring(7);
+        console.log(botID);
+
+    });
 
 });
--- files/socket/index.js
+++ files/socket/index.js
@@ -1,10 +1,11 @@
 module.exports = function (socket, ioCache, io, session, web, app, user, permLevel) {
 
-    console.log(ioCache);
-    console.log(user);
-
-    socket.on("disconnect", (reason) => {
+    // Disconnected
+    socket.on('disconnect', (reason) => {
         console.log(reason); // "ping timeout"
     });
+
+    // Connected
+    socket.emit('discordConnected', user);
 
 };
