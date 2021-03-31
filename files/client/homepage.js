1bf7b321214ebe3d813cc64aeda12e59c9f63bf1
Yasmin Seidel <jackiedevpe@outlook.com.br>
2021-03-31 01:06:41 -0300

    preparing bot data get

--- files/client/homepage.js
+++ files/client/homepage.js
@@ -8,16 +8,23 @@ $(() => {
 
     // Connected
     socket.on('discordConnected', user => {
-        if (firstTime) { firstTime = false; $.LoadingOverlay('hide'); }
+        if (firstTime) { firstTime = false; $.LoadingOverlay('hide'); $('.bot_list').fadeIn(); }
         user = user;
     });
 
     // Select Bot
-    $('[id^="ds_bot_"]').click(function() {
+    $('[id^="ds_bot_"]').click(function () {
 
         // Get Bot ID
         const botID = $(this).attr('id').substring(7);
-        console.log(botID);
+        socket.emit('connectDiscordBot', botID, (data) => {
+
+            /* Quando Carregar. Vamos pegar os dados do bot e colocar um class active aqui e desativar o active do antigo bot. */
+
+            console.log('Bot Data');
+            console.log(data);
+
+        });
 
     });
 
--- files/socket/index.js
+++ files/socket/index.js
@@ -5,6 +5,16 @@ module.exports = function (socket, ioCache, io, session, web, app, user, permLev
         console.log(reason); // "ping timeout"
     });
 
+    // Connect Discord Bot
+    socket.on('connectDiscordBot', function(botID, fn) {
+
+        fn('Mio!');
+
+        // Complete
+        return;
+
+    });
+
     // Connected
     socket.emit('discordConnected', user);
 
