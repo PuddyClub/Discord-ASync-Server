884cfbd3debea6b148385fdad4b67e3d162f9561
Yasmin Seidel <jackiedevpe@outlook.com.br>
2021-04-13 01:11:14 -0300

    channel group sort added

--- files/client/get/channels.js
+++ files/client/get/channels.js
@@ -8,7 +8,55 @@ if (data.success) {
     // Prepare Body
     const body = [];
 
-    console.log(data);
+    // Prepare Groups
+    const groups = [{ id: 0, position: -1, name: '', items: {} }];
+    for (const item in data.result) {
+        if (data.result[item].type !== "category") {
+
+            // Category Item
+            let category;
+
+            // Exist Category
+            if (data.result[item].parentID) {
+
+                // Exist Category
+                category = groups.find(group => group.id === data.result[item].parentID);
+                if (!category) {
+
+                    // Create Category
+                    category = data.result.find(group => group.id === data.result[item].parentID);
+                    groups.push(category);
+
+                }
+
+            }
+
+            // Nope
+            else { category = groups.find(group => group.id === 0); }
+
+            // Insert Into Category
+            if (!category.items) { category.items = {}; }
+            if (!Array.isArray(category.items[data.result[item].type])) { category.items[data.result[item].type] = []; }
+            category.items[data.result[item].type].push(data.result[item]);
+            delete data.result[item].parentID;
+            delete data.result[item].type;
+
+        }
+    }
+
+    // Sort Groups
+    const sortPositions = function (a, b) {
+        return a.position < b.position ? -1 : a.position > b.position ? 1 : 0;
+    };
+
+    groups.sort(sortPositions);
+    for (const item in groups) {
+        for (const group in groups[item].items) {
+            groups[item].items[group].sort(sortPositions);
+        }
+    }
+
+    console.log(groups);
 
     // Modal
     tinyLib.modal({
