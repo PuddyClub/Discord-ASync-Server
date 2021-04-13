e6f5869cd45b46fd9d8b8941d508c215c7694e6e
Yasmin Seidel <jackiedevpe@outlook.com.br>
2021-04-12 22:57:48 -0300

    preparing code base

--- files/client/homepage.js
+++ files/client/homepage.js
@@ -382,7 +382,56 @@ $(() => {
                                 // Success
                                 if (data.success) {
 
+                                    // Prepare Body
+                                    const body = [];
+
                                     console.log(data);
+
+                                    // Modal
+                                    tinyLib.modal({
+                                        dialog: 'modal-lg',
+                                        id: 'role-list',
+                                        title: tinyLang.roles,
+                                        body: tinyLib.table({
+
+                                            // Info
+                                            id: 'roles_list',
+                                            class: 'table-striped',
+                                            responsive: true,
+
+                                            // Head
+                                            thead:
+                                            {
+                                                items: [
+
+                                                    // TDs
+                                                    {
+                                                        items: [
+
+                                                            // Name
+                                                            {
+                                                                isText: true,
+                                                                item: tinyLang.name
+                                                            },
+
+                                                            // Actions
+                                                            {
+                                                                isText: true,
+                                                                item: tinyLang.actions
+                                                            }
+
+                                                        ]
+                                                    }
+
+                                                ]
+                                            },
+
+                                            // Body
+                                            tbody: { items: body }
+
+                                        }),
+                                        footer: [tinyLib.button(tinyLang.close, 'secondary', { 'data-dismiss': 'modal' })]
+                                    });
 
                                 }
 
@@ -392,7 +441,7 @@ $(() => {
                                     // Fail Error Message
                                     tinyLib.modal({
                                         dialog: 'modal-lg',
-                                        id: 'emoji-download-list-error',
+                                        id: 'role-list-error',
                                         title: 'Error!',
                                         body: data.error,
                                         footer: [tinyLib.button(tinyLang.close, 'secondary', { 'data-dismiss': 'modal' })]
@@ -419,7 +468,56 @@ $(() => {
                                 // Success
                                 if (data.success) {
 
+                                    // Prepare Body
+                                    const body = [];
+
                                     console.log(data);
+
+                                    // Modal
+                                    tinyLib.modal({
+                                        dialog: 'modal-lg',
+                                        id: 'channel-list',
+                                        title: tinyLang.channels,
+                                        body: tinyLib.table({
+
+                                            // Info
+                                            id: 'channels_list',
+                                            class: 'table-striped',
+                                            responsive: true,
+
+                                            // Head
+                                            thead:
+                                            {
+                                                items: [
+
+                                                    // TDs
+                                                    {
+                                                        items: [
+
+                                                            // Name
+                                                            {
+                                                                isText: true,
+                                                                item: tinyLang.name
+                                                            },
+
+                                                            // Actions
+                                                            {
+                                                                isText: true,
+                                                                item: tinyLang.actions
+                                                            }
+
+                                                        ]
+                                                    }
+
+                                                ]
+                                            },
+
+                                            // Body
+                                            tbody: { items: body }
+
+                                        }),
+                                        footer: [tinyLib.button(tinyLang.close, 'secondary', { 'data-dismiss': 'modal' })]
+                                    });
 
                                 }
 
@@ -429,7 +527,7 @@ $(() => {
                                     // Fail Error Message
                                     tinyLib.modal({
                                         dialog: 'modal-lg',
-                                        id: 'emoji-download-list-error',
+                                        id: 'channel-list-error',
                                         title: 'Error!',
                                         body: data.error,
                                         footer: [tinyLib.button(tinyLang.close, 'secondary', { 'data-dismiss': 'modal' })]
