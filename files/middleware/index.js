410e5c943cde5b8604592d854531de5df57d93cf
Yasmin Seidel <jackiedevpe@outlook.com.br>
2021-04-12 23:12:53 -0300

    new files added

--- files/middleware/index.js
+++ files/middleware/index.js
@@ -82,6 +82,9 @@ module.exports = async function (resolve, reject, ioCache, discordCfg, webCfg, f
                 .replace('{ { log_update_script } }', fs.readFileSync(path.join(__dirname, '../client/update_log.js'), 'utf8'))
                 .replace('{ { connection } }', fs.readFileSync(path.join(__dirname, '../client/connection.js'), 'utf8'))
                 .replace('{ { select_bot } }', fs.readFileSync(path.join(__dirname, '../client/select_bot.js'), 'utf8'))
+                .replace('{ { get_emojis } }', fs.readFileSync(path.join(__dirname, '../client/get/emojis.js'), 'utf8'))
+                .replace('{ { get_roles } }', fs.readFileSync(path.join(__dirname, '../client/get/roles.js'), 'utf8'))
+                .replace('{ { get_channels } }', fs.readFileSync(path.join(__dirname, '../client/get/channels.js'), 'utf8'))
                 .replace('{ { tools_script } }', fs.readFileSync(path.join(__dirname, '../client/toolsCreator.js'), 'utf8')),
             date: { year: 2021, month: 3, day: 30, hour: 17, minute: 29 },
             timezone: 'America/Sao_Paulo',
