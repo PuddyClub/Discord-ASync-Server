2fecdfe72a4b88cddaf8189348921b980fc97fcb
Yasmin Seidel <jackiedevpe@outlook.com.br>
2021-04-14 18:43:03 -0300

    get global fixed

--- files/middleware/getGlobal.js
+++ files/middleware/getGlobal.js
@@ -4,7 +4,7 @@ module.exports = function (web, fileCfg, callback) {
         // Prepare Global
         req.globalItems = {
             lang: req.i18.getFile('global'),
-            i18List: req.i18.getSelectedLang(), 
+            i18List: req.i18.getSelectedLang(),
             i18ClientRequest: req.i18.getClientWeb(),
             url: { normal: req.url, uri: encodeURIComponent(req.url) },
             csrftoken: req.csrfToken,
@@ -14,11 +14,16 @@ module.exports = function (web, fileCfg, callback) {
             head: fileCfg,
         };
 
-        // Year Footer
-        req.globalItems.year_footer = req.utc_clock.now.year();
-        if (req.globalItems.year_footer !== 2021) {
-            req.globalItems.year_footer = "2021 - " + req.globalItems.year_footer;
-        }
+        // Get Date
+        try {
+
+            // Year Footer
+            req.globalItems.year_footer = req.utc_clock.now.year();
+            if (req.globalItems.year_footer !== 2021) {
+                req.globalItems.year_footer = "2021 - " + req.globalItems.year_footer;
+            }
+
+        } catch (err) { }
 
         // Complete
         callback(req, res, next);
