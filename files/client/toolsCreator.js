3eebb4ac3b39a09d289e13537915adcabf7abddc
Yasmin Seidel <jackiedevpe@outlook.com.br>
2021-04-14 00:29:25 -0300

    class name

--- files/client/toolsCreator.js
+++ files/client/toolsCreator.js
@@ -2,7 +2,7 @@
 const toolsCreator = {
 
     // Features Icons
-    featuresIcon: function (features) {
+    featuresIcon: function (features, className) {
 
         // Result
         const result = [];
@@ -17,37 +17,37 @@ const toolsCreator = {
 
                     // Commerce
                     if (features[item] === 'COMMERCE') {
-                        result.push(tinyLib.fontAwesome('fa-store', 'fas').attr('title', tinyLang.commerce).addClass('mr-2'));
+                        result.push(tinyLib.fontAwesome('fa-store', 'fas').attr('title', tinyLang.commerce).addClass(className));
                     }
 
                     // Commerce
                     if (features[item] === 'COMMUNITY') {
-                        result.push(tinyLib.fontAwesome('fa-users', 'fas').attr('title', tinyLang.community).addClass('mr-2'));
+                        result.push(tinyLib.fontAwesome('fa-users', 'fas').attr('title', tinyLang.community).addClass(className));
                     }
 
                     // Commerce
                     if (features[item] === 'DISCOVERABLE') {
-                        result.push(tinyLib.fontAwesome('fa-globe', 'fas').attr('title', tinyLang.discoverable).addClass('mr-2'));
+                        result.push(tinyLib.fontAwesome('fa-globe', 'fas').attr('title', tinyLang.discoverable).addClass(className));
                     }
 
                     // Commerce
                     if (features[item] === 'NEWS') {
-                        result.push(tinyLib.fontAwesome('fa-bullhorn', 'fas').attr('title', tinyLang.news).addClass('mr-2'));
+                        result.push(tinyLib.fontAwesome('fa-bullhorn', 'fas').attr('title', tinyLang.news).addClass(className));
                     }
 
                     // Commerce
                     if (features[item] === 'PARTNERED') {
-                        result.push(tinyLib.fontAwesome('fa-infinity', 'fas').attr('title', tinyLang.partnered).addClass('mr-2'));
+                        result.push(tinyLib.fontAwesome('fa-infinity', 'fas').attr('title', tinyLang.partnered).addClass(className));
                     }
 
                     // Commerce
                     if (features[item] === 'VERIFIED') {
-                        result.push(tinyLib.fontAwesome('fa-certificate', 'fas').attr('title', tinyLang.verified).addClass('mr-2'));
+                        result.push(tinyLib.fontAwesome('fa-certificate', 'fas').attr('title', tinyLang.verified).addClass(className));
                     }
 
                     // Commerce
                     if (features[item] === 'VIP_REGIONS') {
-                        result.push(tinyLib.fontAwesome('fa-star', 'fas').attr('title', tinyLang.vip_regions).addClass('mr-2'));
+                        result.push(tinyLib.fontAwesome('fa-star', 'fas').attr('title', tinyLang.vip_regions).addClass(className));
                     }
 
                 }
