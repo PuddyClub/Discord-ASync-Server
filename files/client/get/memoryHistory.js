974efbc44061afe38b233fa66d5a99f89b519c7f
Yasmin Seidel <jackiedevpe@outlook.com.br>
2021-04-25 14:22:28 -0300

    memory history update file

--- files/client/get/memoryHistory.js
+++ files/client/get/memoryHistory.js
@@ -1,7 +1,34 @@
+const memoryCacheHistory = { usedMem: [], freeMem: [], totalMem: [] };
+
+// Update Chart JS
+const updateMemoryCacheData = function () {
+
+    // Test
+    console.log(memoryCacheHistory);
+
+};
+
+// Open History
+$('[id="openHistoryLog"]').click(function () {
+
+    // Get ID
+    const id = $(this).find('span').attr('id');
+    console.log(id);
+
+});
 
 // Memory Server
 socket.on("machineMemory", (data) => {
+
+    // Update Page
     $("#usedMemory").text(data.usedMem.value);
     $("#freeMemory").text(data.freeMem.value);
     $("#totalMemory").text(data.totalMem.value);
+
+    // Update Values
+    if (Array.isArray(data.history.usedMem)) { memoryCacheHistory.usedMem = data.history.usedMem; }
+    if (Array.isArray(data.history.freeMem)) { memoryCacheHistory.freeMem = data.history.freeMem; }
+    if (Array.isArray(data.history.totalMem)) { memoryCacheHistory.totalMem = data.history.totalMem; }
+    updateMemoryCacheData();
+    
 });
