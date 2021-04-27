3145e61f32149d55c480dd35d0cd5233f2bf8505
Yasmin Seidel <jackiedevpe@outlook.com.br>
2021-04-27 18:35:49 -0300

    preparing firebase loader

--- files/middleware/firebase.js
+++ files/middleware/firebase.js
@@ -1,8 +1,28 @@
 module.exports = function (bot, firebase, fbCfg) {
     return new Promise((resolve, reject) => {
 
+        // Modules
+        const objType = require('@tinypudding/puddy-lib/get/objType');
+
+        // Prepare Config
+        fbCfg = _.defaultsDeep({}, fbCfg, {
+
+
+
+        });
+
+        // Is Object
+        if (objType(fbCfg, 'object')) {
+
+            // Result
+            resolve();
+
+        }
+
+        // Nope
+        else { resolve(); }
+
         // Complete
-        resolve();
         return;
 
     });
