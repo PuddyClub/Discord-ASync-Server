1bf010b1162cc92da65bfbd6ade1f31be576581e
Yasmin Seidel <jackiedevpe@outlook.com.br>
2021-04-28 20:49:38 -0300

    error event added

--- files/firebase/discord/error.js
+++ files/firebase/discord/error.js
@@ -1,11 +1,13 @@
 module.exports = function (cmd, db, cfg) {
     return new Promise((resolve, reject) => {
 
-        // Firebase
-        const db = cfg.app.db.main;
-        
+        // Error
+        const err = cmd[0];
+
+        // Set Event
+        db.event.set({ message: err.message, code: err.code }).then(resolve).catch(reject);
+
         // Complete
-        resolve();
         return;
 
     });
