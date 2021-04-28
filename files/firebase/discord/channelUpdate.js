036239ddfae6619efb92235bbb7f36c2610ff832
Yasmin Seidel <jackiedevpe@outlook.com.br>
2021-04-28 20:25:19 -0300

    channelUpdate

--- files/firebase/discord/channelUpdate.js
+++ files/firebase/discord/channelUpdate.js
@@ -1,11 +1,39 @@
 module.exports = function (cmd, db, cfg) {
     return new Promise((resolve, reject) => {
 
-        // Firebase
-        const db = cfg.app.db.main;
+        // Channel
+        const oldChannel = cmd[0];
+        const newChannel = cmd[1];
+
+        // Data
+        const channelGenerator = require('./global/channel');
+        const oldData = channelGenerator(oldChannel);
+        const newData = channelGenerator(newChannel);
+
+        // Guild Channel
+        if (channel.type !== "dm") {
+
+            // Exist Guild
+            if (channel.guild) {
+
+
+
+            }
+
+        }
+
+        // DM
+        else {
+
+        }
+
+        // Set Event
+        db.event.set({
+            oldChannel: oldData,
+            newChannel: newData
+        }).then(resolve).catch(reject);
 
         // Complete
-        resolve();
         return;
 
     });
