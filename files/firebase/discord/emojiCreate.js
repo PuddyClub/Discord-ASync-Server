55dc1bbf89cc47442f5fdd8bf6117bee78d41e67
Yasmin Seidel <jackiedevpe@outlook.com.br>
2021-04-28 20:44:37 -0300

    emojiCreate

--- files/firebase/discord/emojiCreate.js
+++ files/firebase/discord/emojiCreate.js
@@ -1,11 +1,16 @@
 module.exports = function (cmd, db, cfg) {
     return new Promise((resolve, reject) => {
 
-        // Firebase
-        const db = cfg.app.db.main;
+        // Channel
+        const emoji = cmd[0];
+
+        // Data
+        const data = require('./generator/emoji')(emoji);
+
+        // Set Event
+        db.event.child('emoji').set(data).then(resolve).catch(reject);
 
         // Complete
-        resolve();
         return;
 
     });
