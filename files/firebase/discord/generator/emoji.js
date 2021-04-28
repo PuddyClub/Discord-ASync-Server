843d1f3f56e5883460484ab9829d321126b6dad9
Yasmin Seidel <jackiedevpe@outlook.com.br>
2021-04-28 20:42:44 -0300

    emoji generator added

--- files/firebase/discord/generator/emoji.js
+++ files/firebase/discord/generator/emoji.js
@@ -1,8 +1,32 @@
 module.exports = function (emoji) {
 
     // Data
-    const data = {};
-    
+    const data = {
+        animated: emoji.animated,
+        authorID: emoji.author.id,
+        available: emoji.available,
+        createdAt: emoji.createdAt,
+        createdTimestamp: emoji.createdTimestamp,
+        deletable: emoji.deletable,
+        deleted: emoji.deleted,
+        guildID: emoji.guild.id,
+        id: emoji.id,
+        identifier: emoji.identifier,
+        managed: emoji.managed,
+        name: emoji.name,
+        requiresColons: emoji.requiresColons,
+        url: emoji.url,
+        roles: []
+    };
+
+    // Get Members ID
+    if (emoji.roles) {
+        emoji.roles.forEach(function (value, key) {
+            data.roles.push(key);
+            return;
+        });
+    }
+
     // Complete
     return data;
 
