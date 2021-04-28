71a03e9606e8f2b84c5df0c6f18c0488ba7ab775
Yasmin Seidel <jackiedevpe@outlook.com.br>
2021-04-28 20:21:49 -0300

    channelsCreate, channelDelete, channelPinsUpdate

--- files/firebase/discord/channelDelete.js
+++ files/firebase/discord/channelDelete.js
@@ -1,11 +1,33 @@
 module.exports = function (cmd, db, cfg) {
     return new Promise((resolve, reject) => {
 
-        // Firebase
-        const db = cfg.app.db.main;
+        // Channel
+        const channel = cmd[0];
+
+        // Data
+        const data = require('./global/channel')(channel);
+
+        // Guild Channel
+        if (channel.type !== "dm") {
+
+            // Exist Guild
+            if (channel.guild) {
+
+
+
+            }
+
+        }
+
+        // DM
+        else {
+
+        }
+
+        // Set Event
+        db.event.child('channel').set(data).then(resolve).catch(reject);
 
         // Complete
-        resolve();
         return;
 
     });
--- files/firebase/discord/channelPinsUpdate.js
+++ files/firebase/discord/channelPinsUpdate.js
@@ -1,11 +1,17 @@
 module.exports = function (cmd, db, cfg) {
     return new Promise((resolve, reject) => {
 
-        // Firebase
-        const db = cfg.app.db.main;
+        // Items
+        const channel = cmd[0];
+        const time = cmd[1];
+
+        // Data
+        const data = require('./global/channel')(channel);
+
+        // Set Event
+        db.event.set({ channel: data, time: time }).then(resolve).catch(reject);
 
         // Complete
-        resolve();
         return;
 
     });
