7f38c3b2738ff644b105c59930e49a5483bd561a
Yasmin Seidel <jackiedevpe@outlook.com.br>
2021-04-29 14:30:29 -0300

    value update

--- files/firebase/discord/generator/activities.js
+++ files/firebase/discord/generator/activities.js
@@ -1,30 +1,32 @@
 module.exports = function (activities) {
+    if (activities) {
 
-    // Data
-    const data = {
-        applicationID: activities.applicationID,
-        createdAt: activities.createdAt,
-        createdTimestamp: activities.createdTimestamp,
-        details: activities.details,
-        name: activities.name,
-        party: activities.party,
-        state: activities.state,
-        timestamps: activities.timestamps,
-        type: activities.type,
-        state: activities.url
-    };
+        // Data
+        const data = {
+            applicationID: activities.applicationID,
+            createdAt: activities.createdAt,
+            createdTimestamp: activities.createdTimestamp,
+            details: activities.details,
+            name: activities.name,
+            party: activities.party,
+            state: activities.state,
+            timestamps: activities.timestamps,
+            type: activities.type,
+            state: activities.url
+        };
 
-    // Activities Generator
-    const assetsGenerator = require('./presence/assets');
-    const flagsGenerator = require('./presence/flags');
-    
-    data.flags = flagsGenerator(activities.flags);
-    data.assets = assetsGenerator(activities.assets);
+        // Activities Generator
+        const assetsGenerator = require('./presence/assets');
+        const flagsGenerator = require('./presence/flags');
 
-    // Emoji
-    if (activities.emoji && activities.emoji.id) { data.emojiID = activities.emoji.id; }
+        data.flags = flagsGenerator(activities.flags);
+        data.assets = assetsGenerator(activities.assets);
 
-    // Complete
-    return data;
+        // Emoji
+        if (activities.emoji && activities.emoji.id) { data.emojiID = activities.emoji.id; }
 
+        // Complete
+        return data;
+
+    } else { return null; }
 };
