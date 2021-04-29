438b7e7b2c5e0de160cba91f86d2f4a4d3f61718
Yasmin Seidel <jackiedevpe@outlook.com.br>
2021-04-29 18:11:06 -0300

    messages

--- files/firebase/discord/messageDeleteBulk.js
+++ files/firebase/discord/messageDeleteBulk.js
@@ -1,11 +1,24 @@
 module.exports = function (cmd, db, cfg) {
     return new Promise((resolve, reject) => {
 
-        // Firebase
-        const db = cfg.app.db.main;
+        // Data
+        const msgs = cmd[0];
+        const data = [];
+
+        // Prepare
+        const messageGenerator = require('./generator/message');
+
+        // Get Data
+        msgs.forEach(function (value) {
+            data.push(messageGenerator(value));
+            return;
+        });
         
+
+        // Set Event
+        db.event.set({ messages: data }).then(resolve).catch(reject);
+
         // Complete
-        resolve();
         return;
 
     });
