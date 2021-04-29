6ea4fe992715ba45edab47d2ee7f1140cb478d53
Yasmin Seidel <jackiedevpe@outlook.com.br>
2021-04-29 20:40:14 -0300

    role events

--- files/firebase/discord/roleCreate.js
+++ files/firebase/discord/roleCreate.js
@@ -1,11 +1,17 @@
 module.exports = function (cmd, db, cfg) {
     return new Promise((resolve, reject) => {
 
-        // Firebase
-        const db = cfg.app.db.main;
-        
+        // Message
+        const role = cmd[0];
+
+        // Data
+        const roleGenerator = require('./generator/role');
+        const data = roleGenerator(role);
+
+        // Set Event
+        db.event.set({ role: data }).then(resolve).catch(reject);
+
         // Complete
-        resolve();
         return;
 
     });
--- files/firebase/discord/roleUpdate.js
+++ files/firebase/discord/roleUpdate.js
@@ -1,11 +1,19 @@
 module.exports = function (cmd, db, cfg) {
     return new Promise((resolve, reject) => {
 
-        // Firebase
-        const db = cfg.app.db.main;
-        
+        // Message
+        const oldRole = cmd[0];
+        const newRole = cmd[1];
+
+        // Data
+        const roleGenerator = require('./generator/role');
+        const oldData = roleGenerator(oldRole);
+        const newData = roleGenerator(newRole);
+
+        // Set Event
+        db.event.set({ oldRole: oldData, newRole: newData }).then(resolve).catch(reject);
+
         // Complete
-        resolve();
         return;
 
     });
