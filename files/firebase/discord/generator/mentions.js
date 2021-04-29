4769648e9d05160d1d16666217a8be983ba11407
Yasmin Seidel <jackiedevpe@outlook.com.br>
2021-04-29 17:37:06 -0300

    mentios generator added

--- files/firebase/discord/generator/mentions.js
+++ files/firebase/discord/generator/mentions.js
@@ -3,8 +3,42 @@ module.exports = function (mentions) {
 
         // Data
         const data = {
-
+            everyone: mentions.everyone,
+            roles: [],
+            members: [],
+            channels: []
         };
+
+        // Get Channels
+        if (mentions.channels) {
+            mentions.channels.forEach(function (value, key) {
+                data.channels.push(key);
+                return;
+            });
+        }
+
+        if (mentions.crosspostedChannels) {
+            mentions.crosspostedChannels.forEach(function (value) {
+                data.crosspostedChannels.push(value);
+                return;
+            });
+        }
+        
+        // Members
+        if (mentions.members) {
+            mentions.members.forEach(function (value, key) {
+                data.members.push(key);
+                return;
+            });
+        }
+
+        // Roles
+        if (mentions.roles) {
+            mentions.roles.forEach(function (value, key) {
+                data.roles.push(key);
+                return;
+            });
+        }
 
         // Complete
         return data;
