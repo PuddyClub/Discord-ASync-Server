8519fd342ad301a5b9221626137f44afe5f74109
Yasmin Seidel <jackiedevpe@outlook.com.br>
2021-04-29 15:11:52 -0300

    guildMemberUpdate

--- files/firebase/discord/guildMemberUpdate.js
+++ files/firebase/discord/guildMemberUpdate.js
@@ -2,14 +2,16 @@ module.exports = function (cmd, db, cfg) {
     return new Promise((resolve, reject) => {
 
         // Emoji
-        const member = cmd[0];
+        const oldMember = cmd[0];
+        const newMember = cmd[1];
 
         // Data
         const memberGenerator = require('./generator/member');
-        const data = memberGenerator(member);
+        const oldData = memberGenerator(oldMember);
+        const newData = memberGenerator(newMember);
 
         // Set Event
-        db.event.set({ member: member.id }).then(resolve).catch(reject);
+        db.event.set({oldMember: oldData, newMember: newMember.id}).then(resolve).catch(reject);
 
         // Complete
         return;
