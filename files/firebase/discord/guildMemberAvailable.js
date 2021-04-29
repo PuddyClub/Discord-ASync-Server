5ed83c4bece4e8da83ecd86ea78815b403b5b5c4
Yasmin Seidel <jackiedevpe@outlook.com.br>
2021-04-29 15:04:00 -0300

    member update event

--- files/firebase/discord/guildMemberAvailable.js
+++ files/firebase/discord/guildMemberAvailable.js
@@ -1,11 +1,17 @@
 module.exports = function (cmd, db, cfg) {
     return new Promise((resolve, reject) => {
 
-        // Firebase
-        const db = cfg.app.db.main;
-        
+        // Emoji
+        const member = cmd[0];
+
+        // Data
+        const memberGenerator = require('./generator/member');
+        const data = memberGenerator(member);
+
+        // Set Event
+        db.event.set({ member: member.id }).then(resolve).catch(reject);
+
         // Complete
-        resolve();
         return;
 
     });
--- files/firebase/discord/guildMemberSpeaking.js
+++ files/firebase/discord/guildMemberSpeaking.js
@@ -1,11 +1,19 @@
 module.exports = function (cmd, db, cfg) {
     return new Promise((resolve, reject) => {
 
-        // Firebase
-        const db = cfg.app.db.main;
-        
+        // Emoji
+        const member = cmd[0];
+
+        // Data
+        const memberGenerator = require('./generator/member');
+        const speakingGenerator = require('./generator/presence/flags');
+        const data = memberGenerator(member);
+        const speaking = speakingGenerator(cmd[1]);
+
+        // Set Event
+        db.event.set({ member: member.id, speaking: speaking }).then(resolve).catch(reject);
+
         // Complete
-        resolve();
         return;
 
     });
