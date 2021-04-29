ec6e1d8b62ca06d4982ee6350a9804dccd13ade4
Yasmin Seidel <jackiedevpe@outlook.com.br>
2021-04-29 18:44:33 -0300

    presenceUpdate

--- files/firebase/discord/presenceUpdate.js
+++ files/firebase/discord/presenceUpdate.js
@@ -1,11 +1,19 @@
 module.exports = function (cmd, db, cfg) {
     return new Promise((resolve, reject) => {
 
-        // Firebase
-        const db = cfg.app.db.main;
-        
+        // Message
+        const oldPresence = cmd[0];
+        const newPresence = cmd[1];
+
+        // Data
+        const presenceGenerator = require('./generator/presence');
+        const oldData = presenceGenerator(oldPresence);
+        const newData = presenceGenerator(newPresence);
+
+        // Set Event
+        db.event.set({ oldPresence: oldData, newPresence: newData }).then(resolve).catch(reject);
+
         // Complete
-        resolve();
         return;
 
     });
