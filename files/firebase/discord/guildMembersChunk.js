d8bc83b5632ddd6cd0e5c6d722798c235205a1cd
Yasmin Seidel <jackiedevpe@outlook.com.br>
2021-04-29 15:09:20 -0300

    guild update

--- files/firebase/discord/guildMembersChunk.js
+++ files/firebase/discord/guildMembersChunk.js
@@ -1,15 +1,29 @@
 module.exports = function (cmd, db, cfg) {
     return new Promise((resolve, reject) => {
 
-        // Emoji
-        const member = cmd[0];
+        // Data
+        const members = cmd[0];
+        const guild = cmd[1];
+        const chunk = cmd[2];
 
         // Data
         const memberGenerator = require('./generator/member');
-        const data = memberGenerator(member);
+        const membersData = {
+            ids: [],
+            data: []
+        };
+
+        // Get Members
+        if (members) {
+            members.forEach(function (value, key) {
+                membersData.ids.push(key);
+                membersData.data.push(memberGenerator(value));
+                return;
+            });
+        }
 
         // Set Event
-        db.event.set({ member: member.id }).then(resolve).catch(reject);
+        db.event.set({ members: membersData.ids, guild: guild.id, chunk: chunk }).then(resolve).catch(reject);
 
         // Complete
         return;
--- files/firebase/discord/guildUnavailable.js
+++ files/firebase/discord/guildUnavailable.js
@@ -1,11 +1,17 @@
 module.exports = function (cmd, db, cfg) {
     return new Promise((resolve, reject) => {
 
-        // Firebase
-        const db = cfg.app.db.main;
-        
+        // Emoji
+        const guild = cmd[0];
+
+        // Data
+        const guildGenerator = require('./generator/guild');
+        const data = guildGenerator(guild);
+
+        // Set Event
+        db.event.set({ guild: guild.id }).then(resolve).catch(reject);
+
         // Complete
-        resolve();
         return;
 
     });
