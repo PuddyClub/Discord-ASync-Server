06b113ea6eaf40ac83fc3e65b663f36c54a1a239
Yasmin Seidel <jackiedevpe@outlook.com.br>
2021-04-29 18:39:07 -0300

    message reaction event

--- files/firebase/discord/messageReactionAdd.js
+++ files/firebase/discord/messageReactionAdd.js
@@ -1,11 +1,18 @@
 module.exports = function (cmd, db, cfg) {
     return new Promise((resolve, reject) => {
 
-        // Firebase
-        const db = cfg.app.db.main;
-        
+        // Data
+        const messageReaction = cmd[0];
+        const user = cmd[0];
+
+        // Data
+        const reactionGenerator = require('./generator/reaction');
+        const data = reactionGenerator(messageReaction);
+
+        // Set Event
+        db.event.set({ messageReaction: data, user: user.id }).then(resolve).catch(reject);
+
         // Complete
-        resolve();
         return;
 
     });
--- files/firebase/discord/messageReactionRemoveAll.js
+++ files/firebase/discord/messageReactionRemoveAll.js
@@ -1,11 +1,17 @@
 module.exports = function (cmd, db, cfg) {
     return new Promise((resolve, reject) => {
 
-        // Firebase
-        const db = cfg.app.db.main;
-        
+        // Message
+        const msg = cmd[0];
+
+        // Data
+        const messageGenerator = require('./generator/message');
+        const data = messageGenerator(msg);
+
+        // Set Event
+        db.event.set({ message: data }).then(resolve).catch(reject);
+
         // Complete
-        resolve();
         return;
 
     });
