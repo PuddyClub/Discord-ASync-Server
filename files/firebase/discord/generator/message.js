89b9a4ddbe7616c5da5a7d928da4992f2f4fb08c
Yasmin Seidel <jackiedevpe@outlook.com.br>
2021-04-29 16:59:16 -0300

    message update

--- files/firebase/discord/generator/message.js
+++ files/firebase/discord/generator/message.js
@@ -4,23 +4,23 @@ module.exports = function (msg) {
         // Data
         const data = {
             activity: msg.activity,
+            cleanContent: msg.cleanContent,
+            content: msg.content,
+            createdAt: msg.createdAt,
+            createdTimestamp: msg.createdTimestamp,
+            crosspostable: msg.crosspostable,
+            deletable: msg.deletable,
+            deleted: msg.deleted,
+            editable: msg.editable,
+            editedAt: msg.editedAt,
+            editedTimestamp: msg.editedTimestamp,
+            embeds: msg.embeds,
             code: msg.code,
             code: msg.code,
             code: msg.code,
             code: msg.code,
-            code: msg.code,
-            code: msg.code,
-            code: msg.code,
-            code: msg.code,
-            code: msg.code,
-            code: msg.code,
-            code: msg.code,
-            code: msg.code,
-            code: msg.code,
-            code: msg.code,
-            code: msg.code,
-            code: msg.code,
-            code: msg.code,
+            embeds: [],
+            attachments: [],
         };
 
         // Application Generator
@@ -33,6 +33,9 @@ module.exports = function (msg) {
         // Guild ID
         if (msg.guild && msg.guild.id) { data.guildID = msg.guild.id; }
 
+        // Author ID
+        if (msg.author && msg.author.id) { data.authorID = msg.author.id; }
+
         // Get Attachments
         if (msg.attachments && msg.attachments.cache) {
             const attachmentsGenerator = require('./attachments');
@@ -40,6 +43,14 @@ module.exports = function (msg) {
                 data.attachments.push(attachmentsGenerator(value));
                 return;
             });
+        }
+
+        // Embed
+        if (Array.isArray(msg.embeds)) {
+            const embedsGenerator = require('./embeds');
+            for (const item in msg.embeds) {
+                data.embeds.push(embedsGenerator(msg.embeds[item]));
+            }
         }
 
         // Complete
