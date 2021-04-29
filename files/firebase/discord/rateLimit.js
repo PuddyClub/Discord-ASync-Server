2eb0013606bcc4382134eba126c5037b8e064c49
Yasmin Seidel <jackiedevpe@outlook.com.br>
2021-04-29 18:45:34 -0300

    rateLimit event

--- files/firebase/discord/rateLimit.js
+++ files/firebase/discord/rateLimit.js
@@ -1,11 +1,13 @@
 module.exports = function (cmd, db, cfg) {
     return new Promise((resolve, reject) => {
 
-        // Firebase
-        const db = cfg.app.db.main;
-        
+        // Rate Limit Warn
+        const rateLimitInfo = cmd[0];
+
+        // Set Event
+        db.event.set({ rateLimitInfo: rateLimitInfo }).then(resolve).catch(reject);
+
         // Complete
-        resolve();
         return;
 
     });
