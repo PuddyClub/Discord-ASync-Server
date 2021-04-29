7f154c03a7150a8200de121c41c33060c3670627
Yasmin Seidel <jackiedevpe@outlook.com.br>
2021-04-29 14:35:50 -0300

    voice complete

--- files/firebase/discord/generator/voice.js
+++ files/firebase/discord/generator/voice.js
@@ -3,8 +3,25 @@ module.exports = function (voice) {
 
         // Data
         const data = {
-
+            channelID: voice.channelID,
+            deaf: voice.deaf,
+            id: voice.id,
+            mute: voice.mute,
+            selfDeaf: voice.selfDeaf,
+            selfMute: voice.selfMute,
+            selfVideo: voice.selfVideo,
+            serverDeaf: voice.serverDeaf,
+            serverMute: voice.serverMute,
+            sessionID: voice.sessionID,
+            speaking: voice.speaking,
+            streaming: voice.streaming
         };
+
+        // User ID
+        if (voice.member) { data.userID = voice.member.id; }
+
+        // Guild ID
+        if (voice.guild) { data.guildID = voice.guild.id; }
 
         // Complete
         return data;
