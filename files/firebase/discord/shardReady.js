40230cf4222b7df8c6b5529803edba8a725a6730
Yasmin Seidel <jackiedevpe@outlook.com.br>
2021-04-30 00:21:15 -0300

    shard value fixed

--- files/firebase/discord/shardReady.js
+++ files/firebase/discord/shardReady.js
@@ -3,7 +3,10 @@ module.exports = function (cmd, db, cfg) {
 
         // Data
         const id = cmd[0];
-        const unavailableGuilds = cmd[1];
+        let unavailableGuilds = cmd[1];
+
+        // Fix Value
+        if (typeof unavailableGuilds === "undefined") { unavailableGuilds = null; }
 
         // Set Event
         db.event.set({ id: id, unavailableGuilds: unavailableGuilds }).then(resolve).catch(reject);
