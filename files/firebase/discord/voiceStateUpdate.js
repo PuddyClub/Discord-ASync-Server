ea4a47dedcf84056fb5dd1af849f7843f45ccf12
Yasmin Seidel <jackiedevpe@outlook.com.br>
2021-04-30 00:10:41 -0300

    voiceStateUpdate event

--- files/firebase/discord/voiceStateUpdate.js
+++ files/firebase/discord/voiceStateUpdate.js
@@ -1,11 +1,19 @@
 module.exports = function (cmd, db, cfg) {
     return new Promise((resolve, reject) => {
 
-        // Firebase
-        const db = cfg.app.db.main;
-        
+        // Data
+        const oldState = cmd[0];
+        const newState = cmd[1];
+
+        // Data
+        const voiceGenerator = require('./generator/voice');
+        const oldData = voiceGenerator(oldState);
+        const newData = voiceGenerator(newState);
+
+        // Set Event
+        db.event.set({ newState: newData, oldState: oldData }).then(resolve).catch(reject);
+
         // Complete
-        resolve();
         return;
 
     });
