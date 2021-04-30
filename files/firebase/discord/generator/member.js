fa94813bed02badba64663ae0247c08aa5245a04
Yasmin Seidel <jackiedevpe@outlook.com.br>
2021-04-30 11:11:53 -0300

    member fixed

--- files/firebase/discord/generator/member.js
+++ files/firebase/discord/generator/member.js
@@ -1,48 +1,52 @@
 module.exports = function (member) {
+    if (member) {
 
-    // Data
-    const data = {
-        bannable: member.bannable,
-        deleted: member.deleted,
-        displayColor: member.displayColor,
-        displayHexColor: member.displayHexColor,
-        displayName: member.displayName,
-        guildID: member.guild.id,
-        id: member.id,
-        joinedAt: member.joinedAt,
-        joinedTimestamp: member.joinedTimestamp,
-        kickable: member.kickable,
-        lastMessageChannelID: member.lastMessageChannelID,
-        lastMessageID: member.lastMessageID,
-        manageable: member.manageable,
-        nickname: member.nickname,
-        partial: member.partial,
-        premiumSince: member.premiumSince,
-        premiumSinceTimestamp: member.premiumSinceTimestamp,
-        roles: [],
-    };
+        // Data
+        const data = {
+            bannable: member.bannable,
+            deleted: member.deleted,
+            displayColor: member.displayColor,
+            displayHexColor: member.displayHexColor,
+            displayName: member.displayName,
+            id: member.id,
+            joinedAt: member.joinedAt,
+            joinedTimestamp: member.joinedTimestamp,
+            kickable: member.kickable,
+            lastMessageChannelID: member.lastMessageChannelID,
+            lastMessageID: member.lastMessageID,
+            manageable: member.manageable,
+            nickname: member.nickname,
+            partial: member.partial,
+            premiumSince: member.premiumSince,
+            premiumSinceTimestamp: member.premiumSinceTimestamp,
+            roles: [],
+        };
 
-    // Permission Generator
-    const voiceGenerator = require('./voice');
-    data.voice = voiceGenerator(user.voice);
+        // Guild ID
+        if (member.guild && member.guild.id) { data.guildID = member.guild.id; }
 
-    // Permission Generator
-    const permissionGenerator = require('./permissions');
-    data.permissions = permissionGenerator(user.permissions);
+        // Permission Generator
+        const voiceGenerator = require('./voice');
+        data.voice = voiceGenerator(user.voice);
 
-    // Presence Generator
-    const presenceGenerator = require('./presence');
-    data.presence = presenceGenerator(user.presence);
+        // Permission Generator
+        const permissionGenerator = require('./permissions');
+        data.permissions = permissionGenerator(user.permissions);
 
-    // Get Role ID
-    if (member.roles && member.roles.cache) {
-        member.roles.cache.forEach(function (value, key) {
-            data.roles.push(key);
-            return;
-        });
-    }
+        // Presence Generator
+        const presenceGenerator = require('./presence');
+        data.presence = presenceGenerator(user.presence);
 
-    // Complete
-    return data;
+        // Get Role ID
+        if (member.roles && member.roles.cache) {
+            member.roles.cache.forEach(function (value, key) {
+                data.roles.push(key);
+                return;
+            });
+        }
 
+        // Complete
+        return data;
+
+    } else { return null; }
 };
