b14fb88d607f1a57a41a32165b3716b0fc4a3007
Yasmin Seidel <jackiedevpe@outlook.com.br>
2021-04-29 23:49:24 -0300

    typingStart event

--- files/firebase/discord/typingStart.js
+++ files/firebase/discord/typingStart.js
@@ -1,11 +1,18 @@
 module.exports = function (cmd, db, cfg) {
     return new Promise((resolve, reject) => {
 
-        // Firebase
-        const db = cfg.app.db.main;
-        
+        // Data
+        const channel = cmd[0];
+        const user = cmd[1];
+
+        // Data
+        const userGenerator = require('./generator/user');
+        const userData = userGenerator(user);
+
+        // Set Event
+        db.event.set({ user: userData, channel: channel.id }).then(resolve).catch(reject);
+
         // Complete
-        resolve();
         return;
 
     });
