acbab7c4389f8070863da89064d2854290ddfe91
Yasmin Seidel <jackiedevpe@outlook.com.br>
2021-04-30 01:33:16 -0300

    escape fixed

--- files/firebase/discord/generator/escapeResult.js
+++ files/firebase/discord/generator/escapeResult.js
@@ -1,4 +1,4 @@
-module.exports = function (obj) {
+const escapeValuesHere = (obj) => {
 
     // Obj Type
     const objType = require('@tinypudding/puddy-lib/get/objType');
@@ -8,8 +8,21 @@ module.exports = function (obj) {
         Object.keys(obj).forEach(key => {
             if (obj[key] === undefined) {
                 delete obj[key];
+            } else if (objType(obj[key], 'object') || Array.isArray(obj[key])) {
+                obj[key] = escapeValuesHere(obj[key]);
             }
         });
+    }
+
+    // Array
+    else if (Array.isArray(obj)) {
+        for (const key in obj) {
+            if (obj[key] === undefined) {
+                delete obj[key];
+            } else if (objType(obj[key], 'object') || Array.isArray(obj[key])) {
+                obj[key] = escapeValuesHere(obj[key]);
+            }
+        }
     }
 
     // Nothing
@@ -20,3 +33,5 @@ module.exports = function (obj) {
     return obj;
 
 };
+
+module.exports = function (obj) { return escapeValuesHere(obj); };
