2c6fbc772774085f8a87a0645c7f8da26b362135
Yasmin Seidel <jackiedevpe@outlook.com.br>
2021-05-01 01:03:24 -0300

    voice update event

--- files/firebase/discord/voiceStateUpdate.js
+++ files/firebase/discord/voiceStateUpdate.js
@@ -10,6 +10,15 @@ module.exports = function (cmd, db, cfg) {
         const oldData = db.escape(voiceGenerator(oldState));
         const newData = db.escape(voiceGenerator(newState));
 
+        // Member ID
+        const memberID = db.escape(newData.userID);
+
+        // Guild ID
+        const guildID = db.escape(newData.guildID);
+
+        // Update Channel
+        await db.root.child('guilds').child(guildID).child('members').child(memberID).child('voice').update(newData);
+
         // Set Event
         db.event.set({ newState: newData, oldState: oldData }).then(resolve).catch(reject);
 
