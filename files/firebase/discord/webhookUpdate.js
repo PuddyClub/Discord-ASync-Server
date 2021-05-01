eccad781de44378fcfadbc3894f321442d3ae063
Yasmin Seidel <jackiedevpe@outlook.com.br>
2021-05-01 01:05:22 -0300

    webhook event updated

--- files/firebase/discord/webhookUpdate.js
+++ files/firebase/discord/webhookUpdate.js
@@ -7,6 +7,25 @@ module.exports = function (cmd, db, cfg) {
         // Data
         const data = db.escape(require('./generator/channel')(channel));
 
+        // Guild Channel
+        if (channel.type !== "dm") {
+
+            // Exist Guild
+            if (channel.guild) {
+
+                // Channel ID
+                const channelID = db.escape(channel.id);
+
+                // Guild ID
+                const guildID = db.escape(channel.guild.id);
+
+                // Update Channel
+                await db.root.child('guilds').child(guildID).child('channels').child(channelID).update(data);
+
+            }
+
+        }
+
         // Set Event
         db.event.child('channel').set(data).then(resolve).catch(reject);
 
