dc9c7926d31f41c3ea0d1ef8b059e04b3193dba8
Yasmin Seidel <jackiedevpe@outlook.com.br>
2021-05-01 01:00:10 -0300

    role events

--- files/firebase/discord/roleDelete.js
+++ files/firebase/discord/roleDelete.js
@@ -8,6 +8,15 @@ module.exports = function (cmd, db, cfg) {
         const roleGenerator = require('./generator/role');
         const data = db.escape(roleGenerator(role));
 
+        // Role ID
+        const roleID = db.escape(role.id);
+
+        // Guild ID
+        const guildID = db.escape(role.guild.id);
+
+        // Update Channel
+        await db.root.child('guilds').child(guildID).child('roles').child(roleID).remove();
+
         // Set Event
         db.event.set({ role: data }).then(resolve).catch(reject);
 
--- files/firebase/discord/roleUpdate.js
+++ files/firebase/discord/roleUpdate.js
@@ -10,8 +10,17 @@ module.exports = function (cmd, db, cfg) {
         const oldData = db.escape(roleGenerator(oldRole));
         const newData = db.escape(roleGenerator(newRole));
 
+        // Role ID
+        const roleID = db.escape(newRole.id);
+
+        // Guild ID
+        const guildID = db.escape(newRole.guild.id);
+
+        // Update Channel
+        await db.root.child('guilds').child(guildID).child('roles').child(roleID).update(newData);
+
         // Set Event
-        db.event.set({ oldRole: oldData, newRole: newData }).then(resolve).catch(reject);
+        db.event.set({ oldRole: oldData, newRole: db.escape(newRole.id) }).then(resolve).catch(reject);
 
         // Complete
         return;
